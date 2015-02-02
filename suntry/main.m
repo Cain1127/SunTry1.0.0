ada849a2aa2756c6aec8f9076ba300768903be2a
ysmeng <49427823@163.com>
2015-02-02 18:58:48 +0800

    创建工程-杨声孟

---
+++ suntry/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  suntry
+//
+//  Created by ysmeng on 15/2/2.
+//  Copyright (c) 2015年 广州七升网络科技有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "QSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([QSAppDelegate class]));
+    }
+}
---
+++ suntryTests/suntryTests.m
@@ -0,0 +1,34 @@
+//
+//  suntryTests.m
+//  suntryTests
+//
+//  Created by ysmeng on 15/2/2.
+//  Copyright (c) 2015年 广州七升网络科技有限公司. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface suntryTests : XCTestCase
+
+@end
+
+@implementation suntryTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
