55873fdc6d858cc63a35c57a93f0825b695393e0
wsp2046 <1945959282@qq.com>
2015-03-02 09:34:33 +0800

    添加首页广告网络接口--王树朋

---
+++ suntry/Model/Configuration/QSBannerReturnData.m
@@ -0,0 +1,13 @@
+//
+//  QSBannerReturnData.m
+//  suntry
+//
+//  Created by 王树朋 on 15/3/2.
+//  Copyright (c) 2015年 广州七升网络科技有限公司. All rights reserved.
+//
+
+#import "QSBannerReturnData.h"
+
+@implementation QSBannerReturnData
+
+@end
